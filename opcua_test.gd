cca84bd15dd03d0b7ac92714b2fadd25f132a399
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2025-03-17 12:10:11 -0400

    update gdext DLL

--- opcua_test.gd
+++ opcua_test.gd
@@ -1,6 +1,11 @@
 @tool
 extends Node3D
 
-func _ready() -> void:
+@export var run_test := false:
+	set(value):
+		_run_test()
+
+func _run_test() -> void:
+	# removed from OIPComms extension
 	#OIPComms.opc_ua_test()
 	pass
